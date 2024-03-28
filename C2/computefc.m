b41d84c1c15758f02d73c3f71ff915e345a74d7b
Alexis Contreras R <ale.aicr@gmail.com>
2024-03-28 15:32:39 -0300

    Update and rename fc_Mander.m to computefc.m
    computefc() computes the stress of concrete fibers

---
+++ C2/computefc.m
@@ -0,0 +1,18 @@
+function fc = comptuefc(concrete, ec, alternative)
+  % This function computes the concrete's stress in function of the concret's deformation using
+  % concrete: struct with the following concrete's properties:
+  %    E: Young Modulus
+  %    eo: 
+  % ec: deformation of the fiber (scalar or vector)
+  % alternative: char with the name of the method to estimate the stress ('mander', '...')
+
+  % Choose alternative
+  if isequal(loswer(alternative), 'mander')
+  
+  elseif isequal(lower(alternative), '')
+
+  else
+  
+  end
+end
+
